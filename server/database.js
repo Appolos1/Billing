ce497cc084a171ac38f604b2042520fe1e9d9432
Appolos1 <112808243+Appolos1@users.noreply.github.com>
2026-03-10 18:11:28 +0300

    Add files via upload

--- server/database.js
+++ server/database.js
@@ -8,7 +8,7 @@ const Database = require('better-sqlite3');
 const path     = require('path');
 const fs       = require('fs');
 
-const DB_DIR  = path.join(__dirname, 'data');
+const DB_DIR  = process.env.NODE_ENV === 'production' ? '/data' : path.join(__dirname, 'data');
 const DB_FILE = path.join(DB_DIR, 'billing.db');
 
 if (!fs.existsSync(DB_DIR)) fs.mkdirSync(DB_DIR, { recursive: true });
